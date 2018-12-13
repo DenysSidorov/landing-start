049f59c7da0739aa429d93caa7ae4c8a52417126
Denys Sidorov <000scorpions000@gmail.com>
2018-12-14 00:03:44 +0200

    fix mobile version

--- src/native-js/mobile-menu.js
+++ src/native-js/mobile-menu.js
@@ -4,10 +4,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
   if(mobMenuIco && mobMenu){
     mobMenuIco.addEventListener('click', (e)=>{
       if(mobMenu.style.transform == 'translateX(0px)'){
+        mobMenuIco.setAttribute('src', '/static-img/hum.png')
         mobMenu.style.transform = "translateX(-5000px)";
       } else {
+        mobMenuIco.setAttribute('src', '/static-img/close.png')
         mobMenu.style.transform = "translateX(0px)";
       }
     })
+
+
+
   }
 });
